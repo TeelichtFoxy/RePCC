e860f0b1558eb1f2dd5fe3de47f0138575d10f60
teelichtfoxy <elias@skibb.de>
2025-10-07 12:48:24 +0200

    MacOS App angefangen - GUI

---
+++ Windows/REPCC/REPCC/REPCCApp.swift
@@ -0,0 +1,17 @@
+//
+//  REPCCApp.swift
+//  REPCC
+//
+//  Created by Elias Skibb on 06.10.25.
+//
+
+import SwiftUI
+
+@main
+struct REPCCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
